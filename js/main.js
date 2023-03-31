60a6dedbf9e20e12e2f365db19e947ad65e4487d
kimhyeongs <khsh0320@naver.com>
2023-03-31 10:28:41 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEL = document.querySelector('.search');
-const searchInputEL = searchEL.querySelector('input');
-
-searchEL.addEventListener('click', function () {
-  searchInputEL.focus();
-});
-
-searchInputEL.addEventListener('focus', function(){
-  searchEL.classList.add('focused');
-  searchInputEL.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEL.addEventListener('blur', function(){
-  searchEL.classList.remove('focused');
-  searchInputEL.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -144,7 +127,4 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller()); 
 });
 
-// 날짜 시간 이용
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
